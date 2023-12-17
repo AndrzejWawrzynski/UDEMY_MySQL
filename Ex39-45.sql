ccd9e963ab2fbe2b93dbd4a3670871dfc1c7b9ac
Andrzej Wawrzyński <wandrzej@interia.pl>
2023-12-17 19:32:48 +0100

    update Ex39-45

--- Ex39-45.sql
+++ Ex39-45.sql
@@ -12,6 +12,20 @@ WHERE ksiazki.id = ksiazki_id
 GROUP BY zamowienia_id;
 
 /*
+Ex40
+Wypisz imię i nazwisko użytkowników oraz ksiązki jakie zamówili.
+*/
+
+SELECT
+	uzytkownicy.imie,
+    uzytkownicy.nazwisko,
+    ksiazki.tytul 'tytuł książki'
+FROM uzytkownicy
+	JOIN zamowienia ON uzytkownicy.id = zamowienia.uzytkownicy_id
+    JOIN koszyk ON zamowienia.id = koszyk.zamowienia_id
+    JOIN ksiazki ON koszyk.ksiazki_id = ksiazki.id;
+    
+/*
 Ex41
 Wylistuj numery zamówień i zamówione książki.
 */
