1047fb918b55931a33499e0dc3fbaf0b4a625bf1
Andrzej Wawrzyński <wandrzej@interia.pl>
2023-12-17 19:34:10 +0100

    update Ex39-45

--- Ex39-45.sql
+++ Ex39-45.sql
@@ -6,7 +6,7 @@ jakie zostały w ramach każdego zamówienia kupione.
 
 SELECT 
 	zamowienia_id, 
-    COUNT(*) AS ilosc_ksiazek
+	COUNT(*) AS ilosc_ksiazek
 FROM ksiazki, koszyk 
 WHERE ksiazki.id = ksiazki_id 
 GROUP BY zamowienia_id;
@@ -18,8 +18,8 @@ Wypisz imię i nazwisko użytkowników oraz ksiązki jakie zamówili.
 
 SELECT
 	uzytkownicy.imie,
-    uzytkownicy.nazwisko,
-    ksiazki.tytul 'tytuł książki'
+	uzytkownicy.nazwisko,
+	ksiazki.tytul 'tytuł książki'
 FROM uzytkownicy
 	JOIN zamowienia ON uzytkownicy.id = zamowienia.uzytkownicy_id
     JOIN koszyk ON zamowienia.id = koszyk.zamowienia_id
