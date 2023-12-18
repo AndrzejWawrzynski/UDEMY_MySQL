6296c8ee88ed7463ac83d589dbe963b3e5eb1da0
Andrzej Wawrzyński <wandrzej@interia.pl>
2023-12-18 15:29:15 +0100

    Ex42-44

--- Ex39-45.sql
+++ Ex39-45.sql
@@ -37,5 +37,66 @@ FROM koszyk ko
 JOIN ksiazki ks
 ON ko.ksiazki_id = ks.id;
 
+/*
+Ex42
+Znajdz numery zamówień w których zamówiono książkę o Harrym Potterze.
+*/
+
+SELECT
+	zamowienia_id 'numer zamówienia'
+	,tytul
+FROM koszyk
+JOIN ksiazki
+ON ksiazki_id = ksiazki.id
+WHERE tytul LIKE '%Harry Potter%';
+
+/*
+Ex43
+Wypisz adresy email użytkowników, którzy nie złożyli jeszcze zamówienia w sklepie
+aby wysłać im kupon promocyjny na pierwsze zakupy.
+*/
+
+SELECT 	
+	email
+FROM zamowienia z
+RIGHT JOIN uzytkownicy u
+ON z.uzytkownicy_id = u.id
+WHERE z.id IS NULL;
+
+-- OR
+
+SELECT 	
+	uzytkownicy.email
+FROM zamowienia
+RIGHT JOIN uzytkownicy
+ON zamowienia.uzytkownicy_id = uzytkownicy.id
+WHERE zamowienia.id IS NULL;
+
+-- OR
+
+SELECT 	
+	uzytkownicy.email
+FROM zamowienia
+RIGHT JOIN uzytkownicy
+ON zamowienia.uzytkownicy_id = uzytkownicy.id
+WHERE zamowienia.uzytkownicy_id IS NULL;
+
+/*
+Ex44
+Wylistuj wszystkich użytkowników z Warszawy i powiąż ich z zamówieniami.
+*/
+
+SELECT
+	*
+FROM uzytkownicy
+LEFT JOIN zamowienia
+ON uzytkownicy.id = zamowienia.uzytkownicy_id
+WHERE uzytkownicy.adres_miasto LIKE 'Warszawa';
+
+/*
+Ex45
+
+
+
 
 
